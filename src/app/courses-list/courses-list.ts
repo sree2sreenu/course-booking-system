5397fedc1c88fd4a430b36214c9c3700c6d715e4
Sreenivas Kuruva <sree2sreenu@gmail.com>
2025-06-15 23:54:39 +0530

    property and event  binding

--- src/app/courses-list/courses-list.ts
+++ src/app/courses-list/courses-list.ts
@@ -10,9 +10,13 @@ import { PristineChangeEvent } from '@angular/forms';
 export class CoursesList {
   title = 'Available Courses';
   courses = [
-    { id: 1, name: 'Angular Basics', description: 'Learn the fundamentals of Angular.', price: 100, date: new Date() },
-    { id: 2, name: 'Advanced Angular', description: 'Deep dive into Angular features.', price: 150, date: new Date() },
-    { id: 3, name: 'Angular and RxJS', description: 'Integrate RxJS with Angular applications.', price: 200, date: new Date() }
+    { id: 1, name: 'Angular Basics', description: 'Learn the fundamentals of Angular.', price: 100, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
+    { id: 2, name: 'Advanced Angular', description: 'Deep dive into Angular features.', price: 150, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
+    { id: 3, name: 'Angular and RxJS', description: 'Integrate RxJS with Angular applications.', price: 200, date: "06/15/2025", soldOut: true, img:"angular-logo.png" }
   ];
+
+  viewDetails(CourseName: string): void {
+    alert(`Details for ${CourseName}`);
+  }
 
 }
