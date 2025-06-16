e0813e990ed80f238480283f000719d13f77e2b3
Sreenivas Kuruva <sree2sreenu@gmail.com>
2025-06-17 00:27:50 +0530

    if and for statements

--- src/app/courses-list/courses-list.ts
+++ src/app/courses-list/courses-list.ts
@@ -11,9 +11,9 @@ import { PristineChangeEvent } from '@angular/forms';
 export class CoursesList implements OnInit {
   title = 'Available Courses';
   courses = [
-    { id: 1, name: 'Angular Basics', description: 'Learn the fundamentals of Angular.', price: 100, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
+    { id: 1, name: 'Angular Basics', description: 'Learn the fundamentals of Angular.', price: 50, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
     { id: 2, name: 'Advanced Angular', description: 'Deep dive into Angular features.', price: 150, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
-    { id: 3, name: 'Angular and RxJS', description: 'Integrate RxJS with Angular applications.', price: 200, date: "06/15/2025", soldOut: true, img:"angular-logo.png" }
+    { id: 3, name: 'Angular and RxJS', description: 'Integrate RxJS with Angular applications.', price: 90, date: "06/15/2025", soldOut: true, img:"rxjs-logo.png" }
   ];
 
   ngOnInit() {
