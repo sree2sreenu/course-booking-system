565ea173f62c14486826855562b62f66656dfbe2
Sreenivas Kuruva <sree2sreenu@gmail.com>
2025-06-16 23:41:36 +0530

    ngOnInit added

--- src/app/courses-list/courses-list.ts
+++ src/app/courses-list/courses-list.ts
@@ -1,5 +1,5 @@
 import { NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PristineChangeEvent } from '@angular/forms';
 
 @Component({
@@ -8,13 +8,18 @@ import { PristineChangeEvent } from '@angular/forms';
   templateUrl: './courses-list.html',
   styleUrl: './courses-list.css'
 })
-export class CoursesList {
+export class CoursesList implements OnInit {
   title = 'Available Courses';
   courses = [
     { id: 1, name: 'Angular Basics', description: 'Learn the fundamentals of Angular.', price: 100, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
     { id: 2, name: 'Advanced Angular', description: 'Deep dive into Angular features.', price: 150, date: "06/15/2025", soldOut: false, img:"angular-logo.png" },
     { id: 3, name: 'Angular and RxJS', description: 'Integrate RxJS with Angular applications.', price: 200, date: "06/15/2025", soldOut: true, img:"angular-logo.png" }
   ];
+
+  ngOnInit() {
+    // Initialization logic can go here
+    console.log('CoursesList component initialized');
+  }
 
   viewDetails(CourseName: string): void {
     alert(`Details for ${CourseName}`);
