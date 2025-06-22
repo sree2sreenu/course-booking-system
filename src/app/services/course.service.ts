b94d3daf2168465f9b59def78bb5916246878c0a
Sreenivas Kuruva <sree2sreenu@gmail.com>
2025-06-22 07:57:29 +0530

    http get , post course methods added

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -8,18 +8,18 @@ import { Observable } from 'rxjs';
 })
 export class CourseService {
 
-   private courses: Course[] = [
-    { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png', onSale: false },
-    { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10', soldOut: true, img: 'angular-logo.png', onSale: true },
-    { id: 3, title: 'RxJS Fundamentals', description: 'Asynchronous data streams', price: 45, date: '2025-05-05', img: 'rxjs-logo.png', soldOut: false, onSale: true }
-  ];
-
-private baseUrl="http://localhost:3000"
+  private baseUrl = "http://localhost:3000"
   constructor(private http: HttpClient) { }
 
-  getCourses(): Observable<Course[]>{
-  return this.http.get<Course[]>(`${this.baseUrl}/courses`);
-}
+  //GET all courses
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseUrl}/courses`);
+  }
+
+  //POST a new course
+  addCourse(course: Course): Observable<Course> {
+    return this.http.post<Course>(`${this.baseUrl}/courses`, course);
+  }
 
 }
 
