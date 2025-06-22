6f48006d7228c9a1c53e20aa2306a983dd64d85b
Sreenivas Kuruva <sree2sreenu@gmail.com>
2025-06-22 12:09:15 +0530

    router module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { CoursesList } from './courses-list/courses-list';
 import { CourseDetails } from './course-details/course-details';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CurrencyPipe,RouterOutlet],
+  imports: [CurrencyPipe,RouterModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
